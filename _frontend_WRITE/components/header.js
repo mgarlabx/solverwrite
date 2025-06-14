eedb5b4b1ffa169692750ff05d7ee490fe5e7506
Mauricio Garcia <mauricio@solverank.tech>
2025-06-14 16:23:36 -0300

    .

--- _frontend_WRITE/components/header.js
+++ _frontend_WRITE/components/header.js
@@ -9,7 +9,7 @@ const header = {
         this.dom.innerHTML = `
             <div class="d-flex justify-content-between header fixed-top p-2 border" style="background-color: #e9f0ff;">
                 <div class="d-flex justify-content-between" style="max-width:320px">    
-                    <h4 class="p-2" id="header-books"><img width="30" src="images/cube.png"> Solverbooks Author</h4>
+                    <h4 class="p-2" id="header-books"><img width="30" src="images/cube.png"> SolverWrite</h4>
                 </div>
             </div>
         `;
